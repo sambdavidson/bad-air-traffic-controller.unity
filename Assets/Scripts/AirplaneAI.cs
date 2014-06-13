8f2c7db3c2e6a0eefa2ee58903581bb2c01cfc84
Samuel Davidson <samdamana@gmail.com>
2014-06-13 00:03:37 -0600

    Initial project upload to github

---
+++ Assets/Scripts/AirplaneAI.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirplaneAI : MonoBehaviour {
+	//Airplane Details
+	public string airplaneTitle = "Default Airplane";
+	public int maximumPassengers;
+	public int baseWeight = 100000;
+	public int fuelCapacity = 10000;
+	public int costPerUnit = 70000000;
+	public Texture airplaneImage;
+
+	//Selection Variables
+	public Shader selectedShader;
+	private Shader defaultShader;
+	public bool isSelected;
+
+	//Detail Panel Objects
+
+	// Use this for initialization
+	void Start () {
+
+		isSelected = false;
+		defaultShader = this.renderer.material.shader;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isSelected) 
+		{
+			this.renderer.material.shader = selectedShader;
+			EnablePanel();
+
+		} else {
+			this.renderer.material.shader = defaultShader;
+			DisablePanel();
+		}
+	}
+
+	void EnablePanel() {
+
+	}
+	void DisablePanel() {
+
+	}
+
+
+
+}
---
+++ Assets/Scripts/BirdsEyeMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdsEyeMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Arrow Keys
+		if (Input.GetKey (KeyCode.RightArrow) && !Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.position = new Vector3 (this.transform.position.x + 1.0f, this.transform.position.y, this.transform.position.z);
+		} else if(Input.GetKey (KeyCode.LeftArrow) && !Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.position = new Vector3 (this.transform.position.x - 1.0f, this.transform.position.y, this.transform.position.z);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow) && !Input.GetKey (KeyCode.DownArrow)) {
+			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z + 1.0f);
+		} else if(Input.GetKey (KeyCode.DownArrow) && !Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z - 1.0f);
+		}
+
+		//Mouse Wheel Zoom
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y - 2.0f, this.transform.position.z);
+		}
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+			this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y + 2.0f, this.transform.position.z);
+		}
+
+	}
+}
