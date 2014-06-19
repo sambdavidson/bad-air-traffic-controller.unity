d230eab62c950725f4acb57da9a65ebea43d2026
Samuel Davidson <samdamana@gmail.com>
2014-06-18 22:32:28 -0600

    Updates to Airplane Information pane
    Just barely getting it to work

--- Assets/Scripts/AirplaneAI.cs
+++ Assets/Scripts/AirplaneAI.cs
@@ -8,20 +8,24 @@ public class AirplaneAI : MonoBehaviour {
 	public int baseWeight = 100000;
 	public int fuelCapacity = 10000;
 	public int costPerUnit = 70000000;
-	public Texture airplaneImage;
+	public Sprite airplaneImage;
 
 	//Selection Variables
 	public Shader selectedShader;
 	private Shader defaultShader;
 	public bool isSelected;
+	private bool isEnabled = false;
 
 	//Detail Panel Objects
-
+	private PanelRenderer panel;
 	// Use this for initialization
 	void Start () {
 
 		isSelected = false;
 		defaultShader = this.renderer.material.shader;
+
+		panel = new PanelRenderer ();
+		panel.InitializePanel (this.gameObject, airplaneTitle, maximumPassengers, baseWeight, fuelCapacity, costPerUnit, airplaneImage);
 
 	}
 	
@@ -30,19 +34,20 @@ public class AirplaneAI : MonoBehaviour {
 
 		if (isSelected) 
 		{
-			this.renderer.material.shader = selectedShader;
-			EnablePanel();
+			if(!panel.isEnabled) {
+				this.renderer.material.shader = selectedShader;
+				panel.Enable();
+
+			}
+
 
 		} else {
-			this.renderer.material.shader = defaultShader;
-			DisablePanel();
+			if(isEnabled) {
+				this.renderer.material.shader = defaultShader;
+				panel.Disable ();
+			}
+
 		}
-	}
-
-	void EnablePanel() {
-
-	}
-	void DisablePanel() {
 
 	}
 
---
+++ Assets/Scripts/PanelRenderer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelRenderer : MonoBehaviour {
+
+	public GameObject airplaneSpritePrefab;
+	private GameObject airplaneSprite;
+
+	private GameObject parentAirplane;
+	public bool isEnabled = false;
+
+	private string airplaneTitle;
+	private int numPassengers;
+	private int currentWeight;
+	private int currentFuel;
+	private int costPerUnit;
+	private Sprite airplaneImage;
+	// Use this for initialization
+	void Start () {
+		//Detail Panel
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void InitializePanel(GameObject parent, string title, int passengers, int weight, int fuel, int cost, Sprite image) {
+
+		airplaneSprite = (GameObject) Instantiate (airplaneSpritePrefab);
+		airplaneSprite.transform.position = new Vector3 (parentAirplane.transform.position.x,
+		                                                 parentAirplane.transform.position.y + 5.0f,
+		                                                 parentAirplane.transform.position.z);
+		airplaneSprite.GetComponent<SpriteRenderer> ().sprite = airplaneImage;
+		print ("Success");
+		parentAirplane = parent;
+ 		airplaneTitle = title;
+		numPassengers = passengers;
+		currentWeight = weight;
+		currentFuel = fuel;
+		costPerUnit = cost;
+		airplaneImage = image;
+
+	}
+
+	public void Enable() {
+		airplaneSprite.GetComponent<SpriteRenderer> ().enabled = true;
+		isEnabled = true;
+	}
+	public void Disable() {
+		airplaneSprite.GetComponent<SpriteRenderer> ().enabled = false;
+		isEnabled = false;
+		
+	}
+}
